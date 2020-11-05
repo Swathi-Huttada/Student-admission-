1607ccce7e9772e35719db0cb160bb95517b2dbc
Swathi-Huttada <52745868+Swathi-Huttada@users.noreply.github.com>
2020-11-05 17:54:18 +0530

    Add files via upload

---
+++ appconst.h
@@ -0,0 +1,23 @@
+/*DEFINES ALL THE GLOBAL VARIABLES AND STORING THE VALUES
+
+  Author:              Swathi M Huttada
+  Registration number: 201046019
+  Department:          Big data and data analytics
+  Date:                03/11/2020
+
+*/
+
+#ifndef APPCONST_H_INCLUDED
+#define APPCONST_H_INCLUDED
+
+#define STUDENT_NAME_LEN   64
+
+#define PROGRAM_NAME_LEN   32
+#define STUDENT_EMAIL_LEN  64
+#define TOTAL_STUDENT     100
+#define STATUS_LEN         20
+
+#endif
+
+
+
